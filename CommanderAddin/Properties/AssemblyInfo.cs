e97d056306b34464fd6d37a4fe0f2c5983198d00
rickstrahl <rstrahl@west-wind.com>
2020-07-05 21:51:21 -1000

    Update version

--- CommanderAddin/Properties/AssemblyInfo.cs
+++ CommanderAddin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rick Strahl, West Wind Technologies")]
 [assembly: AssemblyProduct("Markdown Monster")]
-[assembly: AssemblyCopyright("© Rick Strahl, West Wind Technologies 2017-2018")]
+[assembly: AssemblyCopyright("© Rick Strahl, West Wind Technologies 2017-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.2")]
+[assembly: AssemblyVersion("0.5.8")]
